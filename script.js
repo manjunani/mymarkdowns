6587ba16cf06c47abaf1bfa6fa29dc587cef95a2
Manjunatha Sai Uppu <manjunatha16.512@gmail.com>
2023-05-31 10:13:35 +0530

    Added Html Code

---
+++ script.js
@@ -0,0 +1,21 @@
+let data = [
+  {
+    name: 'Startups Of the Year 2023',
+    path: 'startups_of_the_year_2023.md',
+  },
+];
+let tabledata = document.getElementById('tabledata');
+console.log(tabledata);
+for(let data in tabledata) {
+    let row = document.createElement('tr');
+    let name = document.createElement('td');
+    let path = document.createElement('td');
+    let a = document.createElement('a');
+    a.href = data.path
+    console.log(a)
+    name.innerHTML = data.name;
+    path.innerHTML = a
+    row.appendChild(name);
+    row.appendChild(path);
+    tabledata.appendChild(row);
+}
